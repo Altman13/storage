aeed7016be0cb000354865496b4c0977a39fae4e
Altman13 <georgekhromov1982@gmail.com>
2019-01-06 16:04:00 +0300

    c# arduino+multithreading

---
+++ Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace it_storage
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.empl_name = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.barcode = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.department = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.status = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Telephone = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SendDataBtn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(348, 399);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(133, 27);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Добавить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.empl_name,
+            this.barcode,
+            this.department,
+            this.status,
+            this.Telephone});
+            this.dataGridView1.GridColor = System.Drawing.SystemColors.HotTrack;
+            this.dataGridView1.Location = new System.Drawing.Point(9, 10);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(746, 384);
+            this.dataGridView1.TabIndex = 5;
+            // 
+            // empl_name
+            // 
+            this.empl_name.HeaderText = "ФИО сотрудника";
+            this.empl_name.Name = "empl_name";
+            // 
+            // barcode
+            // 
+            this.barcode.HeaderText = "Уникальный номер";
+            this.barcode.Name = "barcode";
+            // 
+            // department
+            // 
+            this.department.HeaderText = "Отдел";
+            this.department.Name = "department";
+            // 
+            // status
+            // 
+            this.status.HeaderText = "Статус сотрудника";
+            this.status.Name = "status";
+            // 
+            // Telephone
+            // 
+            this.Telephone.HeaderText = "Телефон";
+            this.Telephone.Name = "Telephone";
+            // 
+            // button3
+            // 
+            this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button3.Location = new System.Drawing.Point(622, 399);
+            this.button3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(133, 27);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Удалить";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // SendDataBtn
+            // 
+            this.SendDataBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.SendDataBtn.Location = new System.Drawing.Point(485, 399);
+            this.SendDataBtn.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.SendDataBtn.Name = "SendDataBtn";
+            this.SendDataBtn.Size = new System.Drawing.Size(133, 27);
+            this.SendDataBtn.TabIndex = 7;
+            this.SendDataBtn.Text = "Передать";
+            this.SendDataBtn.UseVisualStyleBackColor = true;
+            this.SendDataBtn.Click += new System.EventHandler(this.SendDataBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(764, 436);
+            this.Controls.Add(this.SendDataBtn);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = " ";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn empl_name;
+        private System.Windows.Forms.DataGridViewTextBoxColumn barcode;
+        private System.Windows.Forms.DataGridViewTextBoxColumn department;
+        private System.Windows.Forms.DataGridViewTextBoxColumn status;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Telephone;
+        private System.Windows.Forms.Button SendDataBtn;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Http;
+using System.Web.Script.Serialization;
+using System.IO.Ports;
+using System.Media;
+using System.Net.Http.Headers;
+using System.Threading;
+
+
+namespace it_storage
+{
+
+    public partial class Form1 : Form
+    {
+
+        static string comPortData;
+
+
+        public class Employee
+        {
+            public string fio { get; set; }
+            public string barecode { get; set; }
+            public string name { get; set; }
+            public string rank { get; set; }
+            public string phone { get; set; }
+        }
+
+        public class Com
+        {
+            private SerialPort comPort;
+
+            public Com()
+            {
+                this.comPort = new SerialPort("COM4", 9600, Parity.None, 8, StopBits.One);
+                try
+                {
+                    comPort.Open();
+                }
+                catch (Exception)
+                {
+                    throw new Exception();
+                }
+            }
+
+            public string ComRun()
+            {
+                while (true)
+                {
+                    string employeeSn = comPort.ReadExisting();
+                    if (employeeSn.Length > 13)
+                    {
+                        employeeSn = employeeSn.Substring(1, 12);
+                        //comPort.Close();
+                        if (employeeSn != comPortData)
+                        return employeeSn;
+                    }
+                }
+            }
+        }
+        private static async Task<string> GetPostData(/*string dataComPort*/)
+        {   
+            using (HttpClient client = new HttpClient())
+            {
+                JavaScriptSerializer serializer = new JavaScriptSerializer();
+                List<Employee> employees = new List<Employee>();
+                Employee emp = new Employee();
+                employees.Add(new Employee()
+                {
+                    /*barecode = dataComPort.ToString()*/
+                    barecode = "проверка"
+                });
+                string jsonEmployees = serializer.Serialize(employees);
+                HttpResponseMessage result = await client.PostAsync("http://127.0.0.1:8000/api/v1/employee", new StringContent(jsonEmployees, Encoding.UTF8, "application/json"));
+                string resultContent = await result.Content.ReadAsStringAsync();
+                return resultContent;
+            }
+          
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Com comPort = new Com();
+            TaskScheduler uiContext = TaskScheduler.FromCurrentSynchronizationContext();
+           Task task = Task.Factory.StartNew(() =>
+            {
+                while (true)
+                {
+                    Task<string> promise = Task.Run(() => GetPostData());
+                    string dataComPort = comPort.ComRun();
+                    Task.Factory.StartNew(() =>
+                    {
+                        SystemSounds.Beep.Play();
+                        comPortData = dataComPort;
+                    }, CancellationToken.None, TaskCreationOptions.None, uiContext);
+                }
+            });
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int i = dataGridView1.RowCount - 1;
+            if (i >= 0)
+                this.dataGridView1.Rows.RemoveAt(i);
+        }
+
+        private void SendDataBtn_Click(object sender, EventArgs e)
+        {
+            JavaScriptSerializer serializer = new JavaScriptSerializer();
+            Employee emp = new Employee();
+            string json_employees;
+            //формируем массив с сотрудниками для передачи данных на сервер
+            List<Employee>  employees= new List<Employee>();
+
+            for (int j = 0; j < this.dataGridView1.RowCount; j++)
+            {
+                employees.Add(new Employee() { fio = "fio"+j.ToString(), barecode = "barecode" + j.ToString(), name = "name" + j.ToString(),
+                                               rank = "rank" + j.ToString(), phone = "phone" + j.ToString() });
+            }
+
+           json_employees = serializer.Serialize(employees);
+            /*string getResult = "";
+            Task<string> promise = Task.Run(() => GetPostData(json_employees));
+            getResult = promise.Result;*/
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
